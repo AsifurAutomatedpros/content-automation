9d02bdc966522c8d75cd4607cdceef9640c096c9
asifurbluehorse <140338548+asifurbluehorse@users.noreply.github.com>
2025-06-07 18:09:21 +0530

    build error 2

--- src/processes/3KeyWordExtractor.tsx
+++ src/processes/3KeyWordExtractor.tsx
@@ -1,6 +1,6 @@
 'use client';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 interface KeywordExtractorResponse {
   message: string;
